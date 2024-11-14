206fb3c6117e236f80a8f4cd81204589c250eca7
axel-durand <axeldrd.pavil@gmail.com>
2024-11-14 09:11:46 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
-#### Fonction secondaire
+"""
+Vérifie si une chaine de caractère est un palindrome
+"""
+import string
+def nettoyerchaine(p):
+    """
+    Retourne la chaine de caractère sans accent, sans espace, sans ponctuation et en minuscules
+    """
+    p = p.lower()
+    accents = str.maketrans("àâäéèêëùûüîïôöç","aaaeeeeuuuiiooc")
+    p = p.translate(accents)
+    p = p.translate(str.maketrans("","",string.whitespace + string.punctuation))
+    return p
 
 
 def ispalindrome(p):
-
-    # votre code ici
-    
-    return False
+    """
+    Vérifie si une cahine de caractère est un palindrome
+    """
+    p = nettoyerchaine(p)
+    return p[::-1]==p
 
 #### Fonction principale
 
 
 def main():
-
+    """
+    appelle la fonction ispalindrome
+    """
     # vos appels à la fonction secondaire ici
-
+    print(ispalindrome("faker"))
+    print(ispalindrome("hello"))
+    print(ispalindrome("ici"))
+    print(ispalindrome("ressasser"))
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
         print(s, ispalindrome(s))
 
